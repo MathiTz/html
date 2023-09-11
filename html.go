dc12ec65209756b667020256d93ccf49b6a7814f
Matheus Alves <matheusalves789@gmail.com>
2023-09-10 23:17:39 -0300

    Create html.go

---
+++ html.go
@@ -0,0 +1,23 @@
+package html
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// Titulo - exibe o título de uma página HTML
+func Titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile(`<title>(.*?)</title>`)
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
